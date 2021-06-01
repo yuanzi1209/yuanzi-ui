bd192ef01950974944d9ffbb840898f9c116646f
yuanzi <yuanzi1209@gmail.com>
2021-06-01 16:22:23 +0800

    搭建 html 结构

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  yuanzi
-  <router-link to="/">test</router-link> |
-  <router-link to="/test2">test2</router-link>
-  <hr />
   <router-view />
 </template>
 
@@ -10,13 +6,4 @@
 export default {};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="topnav">
+            <div class="logo"></div>
+            <div class="menu"></div>
+        </div>
+        <div class="banner">
+           <h1>yuanzi-ui</h1>
+           <h2>一个厉害的 UI 框架</h2>
+           <p class="actions">
+               <a href="">GitHub</a>
+               <a href="">开始</a>
+           </p>
+        </div>
+    </div>
+</template>
